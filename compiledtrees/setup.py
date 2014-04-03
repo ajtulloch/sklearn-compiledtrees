ce564f4b45d2e18b39070aed7562834870402adc
Andrew Tulloch <andrew@tullo.ch>
2014-04-03 13:28:58 +0100

    Wno-unused-function

--- compiledtrees/setup.py
+++ compiledtrees/setup.py
@@ -13,7 +13,7 @@ def configuration(parent_package="", top_path=None):
                          sources=["_compiled.c"],
                          include_dirs=[numpy.get_include()],
                          libraries=libraries,
-                         extra_compile_args=["-O3"])
+                         extra_compile_args=["-O3", "-Wno-unused-function"])
     config.add_subpackage("tests")
     return config
 
