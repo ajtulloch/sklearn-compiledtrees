98ea169a6e1d7acb5f22020031be2d4532e31232
Andrew Tulloch <andrew@tullo.ch>
2016-11-23 00:27:11 -0800

    [BPF] Start BPF backend for DecisionTrees
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

---
+++ compiledtrees/bpf.py
@@ -0,0 +1,280 @@
+from sklearn import tree
+import copy
+import numpy as np
+import enum
+import collections
+import networkx as nx
+from sklearn.base import ClassifierMixin
+
+
+class Direction(enum.Enum):
+    LEFT = 0
+    RIGHT = 1
+
+
+class NodeTy(enum.Enum):
+    BRANCH = 0
+    LEAF = 1
+    EXIT = 2
+
+Node = collections.namedtuple("Node", ["ty", "args"])
+Ins = collections.namedtuple("Ins", ["code", "jt", "jf", "k"])
+
+exit_node, accept_node, reject_node = -1, -2, -3
+exit = Node(ty=NodeTy.EXIT, args=())
+accept = Node(ty=NodeTy.LEAF, args=(1,))
+reject = Node(ty=NodeTy.LEAF, args=(0,))
+
+
+def visit(tree, node, visit_leaf, visit_inner, join):
+    if tree.children_left[node] == -1:
+        return join(node, visit_leaf(node), None, None)
+    cur = visit_inner(node)
+    left = visit(
+        tree, tree.children_left[node], visit_leaf, visit_inner, join)
+    right = visit(
+        tree, tree.children_right[node], visit_leaf, visit_inner, join)
+    return join(node, cur, left, right)
+
+
+def construct_cfg(tree, threshold):
+    cfg = nx.DiGraph()
+
+    cfg.add_node(exit_node, ann=exit)
+    cfg.add_node(accept_node, ann=accept)
+    cfg.add_node(reject_node, ann=reject)
+
+    cfg.add_edge(accept_node, exit_node)
+    cfg.add_edge(reject_node, exit_node)
+
+    def visit_leaf(node):
+        negatives, positives = tree.value[node].ravel()
+        probability = float(positives) / (negatives + positives)
+        return reject_node if probability > threshold else accept_node
+
+    def visit_inner(node):
+        feature = tree.feature[node]
+        threshold = tree.threshold[node]
+        return Node(ty=NodeTy.BRANCH, args=(feature, threshold))
+
+    def join(node, cur, left, right):
+        if left is None and right is None:
+            return cur
+        cfg.add_node(node, ann=cur)
+        cfg.add_edge(node, left, data=Direction.LEFT)
+        cfg.add_edge(node, right, data=Direction.RIGHT)
+        return node
+
+    visit(tree, 0, visit_leaf, visit_inner, join)
+    return cfg
+
+
+def construct_node_ret_tys(cfg):
+    node_ret_tys = {}
+    for n in nx.dfs_postorder_nodes(cfg, 0):
+        ty = cfg.node[n]['ann'].ty
+        if ty == NodeTy.EXIT:
+            continue
+        if ty == NodeTy.LEAF:
+            node_ret_tys[n] = {cfg.node[n]['ann']}
+            continue
+        if ty == NodeTy.BRANCH:
+            parents = [node_ret_tys[c] for c in cfg.successors(n)]
+            result = set()
+            for parent in parents:
+                result |= parent
+            node_ret_tys[n] = result
+            continue
+    return node_ret_tys
+
+
+BPF_LD = 0x00
+BPF_JMP = 0x05
+BPF_RET = 0x06
+
+# ld/ldx fields
+BPF_W = 0x00
+BPF_H = 0x08
+BPF_B = 0x10
+BPF_ABS = 0x20
+
+# alu/jmp fields
+BPF_JEQ = 0x10
+BPF_JGT = 0x20
+BPF_K = 0x00
+
+
+def bpf_jump(code, k, jt, jf):
+    return Ins(code, jt, jf, k)
+
+
+def bpf_stmt(code, k):
+    return bpf_jump(code, k, 0, 0)
+
+
+def extract_feature_bpf(feature):
+    offset = feature
+    # offset = OFFSETS[features[feature]]
+    return [
+        bpf_stmt(BPF_W | BPF_LD | BPF_ABS, offset),
+    ]
+
+
+def collapse_cfg(cfg, node_ret_tys):
+    import copy
+    cfg = copy.deepcopy(cfg)
+    for n in list(nx.dfs_postorder_nodes(cfg, 0)):
+        if n not in node_ret_tys or len(node_ret_tys[n]) is not 1:
+            continue
+        if n in (reject_node, accept_node, exit_node):
+            continue
+
+        (retval,) = node_ret_tys[n]
+        (decision,) = retval.args
+        target_node = accept_node if decision == 1 else reject_node
+        predecessors = cfg.predecessors(n)
+        for predecessor in predecessors:
+            cfg.add_edge(predecessor, target_node)
+        cfg.remove_node(n)
+    return cfg
+
+
+def construct_fragments(cfg, node_ret_tys):
+    fragments = {}
+    for n in nx.dfs_postorder_nodes(cfg, 0):
+        ty = cfg.node[n]['ann'].ty
+        if ty == NodeTy.EXIT:
+            fragments[n] = []
+            continue
+        if ty == NodeTy.LEAF:
+            assert n in node_ret_tys and len(node_ret_tys[n]) == 1
+            (retval,) = node_ret_tys[n]
+            (decision,) = retval.args
+            fragments[n] = [bpf_stmt(BPF_RET | BPF_K, decision)]
+            continue
+        if ty == NodeTy.BRANCH:
+            if n in node_ret_tys and len(node_ret_tys[n]) == 1:
+                # Branch - can just return threshold value
+                (retval,) = node_ret_tys[n]
+                (decision,) = retval.args
+                fragments[n] = [bpf_stmt(BPF_RET | BPF_K, decision)]
+                continue
+
+            (feature, threshold) = cfg.node[n]['ann'].args
+            threshold = threshold
+
+            left, right = None, None
+            for (src, dst, direction) in cfg.edges(data='data'):
+                if src != n:
+                    continue
+                if direction == Direction.LEFT:
+                    left = dst
+                    continue
+                if direction == Direction.RIGHT:
+                    right = dst
+            assert left
+            assert right
+            # If true, jump to the right
+            # Note: tests X[f] > threshold, so logic is inverted.
+            # Thus, in the true branch, we jump the entire left subtree,
+            # and on failure we just continue.
+            compare = bpf_jump(
+                BPF_JMP | BPF_JGT | BPF_K, threshold, right, left)
+            frag = extract_feature_bpf(feature) + [compare]
+            fragments[n] = frag
+            continue
+
+    return fragments
+
+
+def dce(cfg, fragments):
+    fragments = copy.deepcopy(fragments)
+    visited = {0}
+    for n in nx.dfs_preorder_nodes(cfg, 0):
+        ty = cfg.node[n]['ann'].ty
+        if ty == NodeTy.BRANCH:
+            jmp = fragments[n][-1]
+            if jmp.code == BPF_JMP | BPF_JGT | BPF_K:
+                visited |= {jmp.jt, jmp.jf}
+
+    for dst in fragments.keys():
+        if dst not in visited:
+            fragments[dst] = []
+    return fragments
+
+
+def linearize(cfg, fragments):
+    inss = []
+    label_offsets = {}
+    for n in nx.dfs_postorder_nodes(cfg.reverse(copy=True), -1):
+        label_offsets[n] = len(inss)
+        if n not in fragments:
+            raise Exception("Error: {}, {}".format(cfg, fragments))
+        inss += fragments[n]
+    return (inss, label_offsets)
+
+
+def assemble(inss, label_offsets):
+    inss = copy.deepcopy(inss)
+    for i, ins in enumerate(inss):
+        if ins.code == BPF_JMP | BPF_JGT | BPF_K:
+            jt_abs = label_offsets[ins.jt]
+            jf_abs = label_offsets[ins.jf]
+            jt_rel = jt_abs - i - 1
+            jf_rel = jf_abs - i - 1
+            assert jt_rel >= 0
+            assert jf_rel >= 0
+            inss[i] = inss[i]._replace(jt=jt_rel, jf=jf_rel)
+    return inss
+
+
+def interpret(inss, features):
+    ip = -1
+    acc = None
+    while True:
+        ip += 1
+        ins = inss[ip]
+        if ins.code == BPF_W | BPF_LD | BPF_ABS:
+            # Load from features
+            acc = features[ins.k]
+            assert inss[ip+1].code == BPF_JMP | BPF_JGT | BPF_K
+            continue
+        elif ins.code == BPF_JMP | BPF_JGT | BPF_K:
+            if acc > ins.k:
+                ip += ins.jt
+            else:
+                ip += ins.jf
+            assert inss[ip+1].code in (BPF_W | BPF_LD | BPF_ABS, BPF_RET | BPF_K)
+            continue
+        elif ins.code == BPF_RET | BPF_K:
+            return ins.k
+        else:
+            raise Exception("Unknown opcode: {}".format(ins))
+
+class BpfClassifierPredictor(ClassifierMixin):
+    def __init__(self, clf, decision_threshold=0.5):
+        if not self.compilable(clf):
+            raise ValueError("Invalid classifier: {}".format(clf))
+
+        cfg = construct_cfg(clf.tree_, decision_threshold)
+        node_ret_tys = construct_node_ret_tys(cfg)
+        fragments = construct_fragments(cfg, node_ret_tys)
+        # fragments = dce(cfg, fragments)
+        inss, label_offsets = linearize(cfg, fragments)
+        assembled_inss = assemble(inss, label_offsets)
+        self.assembled_ins = assembled_inss
+
+    def predict(self, X):
+        (n_samples, n_features) = X.shape
+        Y_pred = np.zeros((n_samples,))
+        for i in range(n_samples):
+            Y_pred[i] = interpret(self.assembled_ins, X[i])
+        # In BPF, we invert the signals (drop is 0, pass is 1).
+        return 1 - Y_pred
+
+    @classmethod
+    def compilable(cls, clf):
+        if isinstance(clf, tree.DecisionTreeClassifier) \
+           and clf.tree_ is not None:
+            return True
+        return False
---
+++ compiledtrees/tests/test_bpf.py
@@ -0,0 +1,446 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import copy
+from sklearn import tree
+import compiledtrees.bpf as bpf
+
+from sklearn.utils.testing import \
+    assert_array_almost_equal, assert_raises, assert_equal
+import numpy as np
+import unittest
+
+REGRESSORS = {
+    # ensemble.GradientBoostingRegressor,
+    # ensemble.RandomForestRegressor,
+    # tree.DecisionTreeRegressor,
+}
+
+CLASSIFIERS = {
+    # ensemble.GradientBoostingClassifier,
+    # ensemble.RandomForestClassifier,
+    lambda: tree.DecisionTreeClassifier(max_depth=2, random_state=3),
+}
+
+
+def pairwise(iterable):
+    import itertools
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def assert_equal_predictions(cls, X, y):
+    clf = cls()
+    clf.fit(X, y)
+    compiled = bpf.BpfClassifierPredictor(clf)
+    predictors = [clf, compiled]
+    predictions = [p.predict(X) for p in predictors]
+    for (p1, p2) in pairwise(predictions):
+        assert_array_almost_equal(p1, p2, decimal=10)
+
+
+class TestBpfUtils(unittest.TestCase):
+    bpf = True
+
+    def setUp(self):
+        import collections
+        T = collections.namedtuple(
+            'T',
+            ['children_left', 'children_right',
+             'feature', 'threshold', 'value'])
+        # Deliberately sparse
+        # 0 -> {4, 5}
+        self.t = T(
+            children_left={0: 4, 4: -1, 5: -1},
+            children_right={0: 5},
+            feature={0: 10},
+            threshold={0: 23.4},
+            value={4: np.asarray([89, 880]), 5: np.asarray([605, 105])}
+        )
+
+    def test_construct_cfg(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        self.assertEqual(
+            cfg.edges(data=True),
+            [
+                (0, -3, {'data': bpf.Direction.LEFT}),
+                (0, -2, {'data': bpf.Direction.RIGHT}),
+                (-2, -1, {}),
+                (-3, -1, {})
+            ])
+
+        self.assertEqual(
+            cfg.nodes(data=True),
+            [
+                (0, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(10, 23.4))}),
+                (-2, {'ann': bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))}),
+                (-1, {'ann': bpf.Node(ty=bpf.NodeTy.EXIT, args=())}),
+                (-3, {'ann': bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,))})
+            ])
+
+    def test_construct_node_ret_tys(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        self.assertEquals(
+            node_ret_tys,
+            {
+                -3: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,))]),
+                -2: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+                0: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                        bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))])
+            })
+
+    def test_collapse_cfg(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        self.assertEqual(
+            sorted(ccfg.nodes(data=True)), sorted(cfg.nodes(data=True)))
+        self.assertEqual(ccfg.edges(data=True), cfg.edges(data=True))
+
+    def test_construct_fragments(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        fragments = bpf.construct_fragments(ccfg, node_ret_tys)
+        self.assertEqual(
+            fragments, {
+                -3: [bpf.Ins(code=bpf.BPF_RET | bpf.BPF_K, jt=0, jf=0, k=0)],
+                -2: [bpf.Ins(code=bpf.BPF_RET | bpf.BPF_K, jt=0, jf=0, k=1)],
+                -1: [],
+                0: [
+                    bpf.Ins(
+                        code=bpf.BPF_W | bpf.BPF_LD | bpf.BPF_ABS,
+                        jt=0, jf=0, k=10),
+                    bpf.Ins(
+                        code=bpf.BPF_JMP | bpf.BPF_JGT | bpf.BPF_K,
+                        jt=-2, jf=-3, k=23.4)
+                ]
+            })
+
+    def test_dce(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        fragments = bpf.construct_fragments(ccfg, node_ret_tys)
+        dead_fragments = copy.deepcopy(fragments)
+        dead_fragments[50] = [None]
+        fragments[50] = []
+        dce_fragments = bpf.dce(cfg, dead_fragments)
+        self.assertEqual(fragments, dce_fragments)
+
+    def test_linearize(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        fragments = bpf.construct_fragments(ccfg, node_ret_tys)
+        fragments = bpf.dce(cfg, fragments)
+        inss, label_offsets = bpf.linearize(ccfg, fragments)
+        self.assertEqual(
+            inss,
+            [
+                bpf.Ins(code=32, jt=0, jf=0, k=10),
+                bpf.Ins(code=37, jt=-2, jf=-3, k=23.4),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=6, jt=0, jf=0, k=1)
+            ])
+        self.assertEqual(label_offsets, {-3: 2, -2: 3, -1: 4, 0: 0})
+
+    def test_assemble(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        fragments = bpf.construct_fragments(ccfg, node_ret_tys)
+        fragments = bpf.dce(cfg, fragments)
+        inss, label_offsets = bpf.linearize(ccfg, fragments)
+        inss = bpf.assemble(inss, label_offsets)
+        self.assertEqual(
+            inss,
+            [
+                bpf.Ins(code=32, jt=0, jf=0, k=10),
+                bpf.Ins(code=37, jt=1, jf=0, k=23.4),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=6, jt=0, jf=0, k=1)
+            ])
+
+    def test_interpret(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        fragments = bpf.construct_fragments(ccfg, node_ret_tys)
+        fragments = bpf.dce(cfg, fragments)
+        inss, label_offsets = bpf.linearize(ccfg, fragments)
+        inss = bpf.assemble(inss, label_offsets)
+        result = bpf.interpret(inss, {10: 25})
+        self.assertEqual(result, 1)
+        result = bpf.interpret(inss, {10: 20})
+        self.assertEqual(result, 0)
+
+
+class TestBpfDeepUtils(unittest.TestCase):
+    bpf = True
+
+    def setUp(self):
+        self.maxDiff = 10000
+        np.random.seed(0)
+        clf = tree.DecisionTreeClassifier(max_depth=4, random_state=3)
+        X = np.random.random_integers(low=-100, high=100, size=(1000, 10))
+        y = np.random.random_integers(low=0, high=1, size=1000)
+        clf.fit(X, y)
+        self.clf = clf
+        self.t = clf.tree_
+
+    def test_construct_cfg(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        self.assertEqual(
+            cfg.edges(data=True),
+            [
+                (0, 16, {'data': bpf.Direction.RIGHT}),
+                (0, 1, {'data': bpf.Direction.LEFT}),
+                (1, 9, {'data': bpf.Direction.RIGHT}),
+                (1, 2, {'data': bpf.Direction.LEFT}),
+                (2, 3, {'data': bpf.Direction.LEFT}),
+                (2, 6, {'data': bpf.Direction.RIGHT}),
+                (3, -2, {'data': bpf.Direction.RIGHT}),
+                (6, -3, {'data': bpf.Direction.RIGHT}),
+                (9, 10, {'data': bpf.Direction.LEFT}),
+                (9, 13, {'data': bpf.Direction.RIGHT}),
+                (10, -2, {'data': bpf.Direction.RIGHT}),
+                (13, -3, {'data': bpf.Direction.LEFT}),
+                (13, -2, {'data': bpf.Direction.RIGHT}),
+                (16, 17, {'data': bpf.Direction.LEFT}),
+                (16, -3, {'data': bpf.Direction.RIGHT}),
+                (17, 18, {'data': bpf.Direction.LEFT}),
+                (17, -2, {'data': bpf.Direction.RIGHT}),
+                (18, -3, {'data': bpf.Direction.RIGHT}),
+                (-3, -1, {}),
+                (-2, -1, {})
+            ])
+        # Always right:
+        # 0 -> 16 -> -3 == 0
+
+        # Always left:
+        # 0 -> 1 -> 2 -> 3 -> -2 == 1
+
+        self.assertEqual(
+            cfg.nodes(data=True),
+            [
+                (0, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(5, 94.0))}),
+                (1, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(6, 10.5))}),
+                (2, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(8, -75.5))}),
+                (3, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(0, -41.5))}),
+                (6, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(5, 80.5))}),
+                (9, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(0, -48.5))}),
+                (10, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(2, 55.5))}),
+                (13, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(9, 33.5))}),
+                (16, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(2, 45.5))}),
+                (17, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(1, 77.0))}),
+                (18, {'ann': bpf.Node(ty=bpf.NodeTy.BRANCH, args=(7, 55.0))}),
+                (-1, {'ann': bpf.Node(ty=bpf.NodeTy.EXIT, args=())}),
+                (-3, {'ann': bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,))}),
+                (-2, {'ann': bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))})
+            ])
+
+    def test_construct_node_ret_tys(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        self.assertEquals(
+            node_ret_tys,
+            {-3: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,))]),
+             -2: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             0: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                     bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             1: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                     bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             2: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                     bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             3: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             6: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,))]),
+             9: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                     bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             10: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             13: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                      bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             16: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                      bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             17: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,)),
+                      bpf.Node(ty=bpf.NodeTy.LEAF, args=(1,))]),
+             18: set([bpf.Node(ty=bpf.NodeTy.LEAF, args=(0,))])})
+
+    def test_collapse_cfg(self):
+        pass
+        # cfg = bpf.construct_cfg(self.t, 0.5)
+        # node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        # ccfg = bpf.collapse_cfg(cfg, node_ret_tys)
+        # self.assertEqual(
+        #     sorted(ccfg.nodes(data=True)), sorted(cfg.nodes(data=True)))
+        # self.assertEqual(ccfg.edges(data=True), [])
+
+    def test_construct_fragments(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        fragments = bpf.construct_fragments(cfg, node_ret_tys)
+        self.assertEqual(
+            fragments,
+            {
+                -3: [bpf.Ins(code=6, jt=0, jf=0, k=0)],
+                -2: [bpf.Ins(code=6, jt=0, jf=0, k=1)],
+                -1: [],
+                0: [bpf.Ins(code=32, jt=0, jf=0, k=5),
+                    bpf.Ins(code=37, jt=16, jf=1, k=94.0)],
+                1: [bpf.Ins(code=32, jt=0, jf=0, k=6),
+                    bpf.Ins(code=37, jt=9, jf=2, k=10.5)],
+                2: [bpf.Ins(code=32, jt=0, jf=0, k=8),
+                    bpf.Ins(code=37, jt=6, jf=3, k=-75.5)],
+                3: [bpf.Ins(code=6, jt=0, jf=0, k=1)],
+                6: [bpf.Ins(code=6, jt=0, jf=0, k=0)],
+                9: [bpf.Ins(code=32, jt=0, jf=0, k=0),
+                    bpf.Ins(code=37, jt=13, jf=10, k=-48.5)],
+                10: [bpf.Ins(code=6, jt=0, jf=0, k=1)],
+                13: [bpf.Ins(code=32, jt=0, jf=0, k=9),
+                     bpf.Ins(code=37, jt=-2, jf=-3, k=33.5)],
+                16: [bpf.Ins(code=32, jt=0, jf=0, k=2),
+                     bpf.Ins(code=37, jt=-3, jf=17, k=45.5)],
+                17: [bpf.Ins(code=32, jt=0, jf=0, k=1),
+                     bpf.Ins(code=37, jt=-2, jf=18, k=77.0)],
+                18: [bpf.Ins(code=6, jt=0, jf=0, k=0)]
+            })
+
+    def test_dce(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        fragments = bpf.construct_fragments(cfg, node_ret_tys)
+        dead_fragments = copy.deepcopy(fragments)
+        dead_fragments[50] = [None]
+        fragments[50] = []
+        dce_fragments = bpf.dce(cfg, dead_fragments)
+        self.assertEqual(fragments, dce_fragments)
+
+    def test_linearize(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        fragments = bpf.construct_fragments(cfg, node_ret_tys)
+        fragments = bpf.dce(cfg, fragments)
+        inss, label_offsets = bpf.linearize(cfg, fragments)
+
+        self.assertEqual(
+            inss,
+            [
+                bpf.Ins(code=32, jt=0, jf=0, k=5),
+                bpf.Ins(code=37, jt=16, jf=1, k=94.0),
+                bpf.Ins(code=32, jt=0, jf=0, k=2),
+                bpf.Ins(code=37, jt=-3, jf=17, k=45.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=1),
+                bpf.Ins(code=37, jt=-2, jf=18, k=77.0),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=32, jt=0, jf=0, k=6),
+                bpf.Ins(code=37, jt=9, jf=2, k=10.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=0),
+                bpf.Ins(code=37, jt=13, jf=10, k=-48.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=9),
+                bpf.Ins(code=37, jt=-2, jf=-3, k=33.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=8),
+                bpf.Ins(code=37, jt=6, jf=3, k=-75.5),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=6, jt=0, jf=0, k=1),
+                bpf.Ins(code=6, jt=0, jf=0, k=1),
+                bpf.Ins(code=6, jt=0, jf=0, k=1)
+            ])
+
+        self.assertEqual(
+            label_offsets,
+            {
+                -3: 16,
+                -2: 19,
+                -1: 20,
+                0: 0,
+                1: 7,
+                2: 13,
+                3: 18,
+                6: 15,
+                9: 9,
+                10: 17,
+                13: 11,
+                16: 2,
+                17: 4,
+                18: 6
+            })
+
+    def test_assemble(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        fragments = bpf.construct_fragments(cfg, node_ret_tys)
+        fragments = bpf.dce(cfg, fragments)
+        inss, label_offsets = bpf.linearize(cfg, fragments)
+        inss = bpf.assemble(inss, label_offsets)
+        self.assertEqual(
+            inss,
+            [
+                bpf.Ins(code=32, jt=0, jf=0, k=5),
+                bpf.Ins(code=37, jt=0, jf=5, k=94.0),
+                bpf.Ins(code=32, jt=0, jf=0, k=2),
+                bpf.Ins(code=37, jt=12, jf=0, k=45.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=1),
+                bpf.Ins(code=37, jt=13, jf=0, k=77.0),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=32, jt=0, jf=0, k=6),
+                bpf.Ins(code=37, jt=0, jf=4, k=10.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=0),
+                bpf.Ins(code=37, jt=0, jf=6, k=-48.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=9),
+                bpf.Ins(code=37, jt=6, jf=3, k=33.5),
+                bpf.Ins(code=32, jt=0, jf=0, k=8),
+                bpf.Ins(code=37, jt=0, jf=3, k=-75.5),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=6, jt=0, jf=0, k=0),
+                bpf.Ins(code=6, jt=0, jf=0, k=1),
+                bpf.Ins(code=6, jt=0, jf=0, k=1),
+                bpf.Ins(code=6, jt=0, jf=0, k=1)
+            ])
+
+    def test_interpret(self):
+        cfg = bpf.construct_cfg(self.t, 0.5)
+        node_ret_tys = bpf.construct_node_ret_tys(cfg)
+        fragments = bpf.construct_fragments(cfg, node_ret_tys)
+        fragments = bpf.dce(cfg, fragments)
+        inss, label_offsets = bpf.linearize(cfg, fragments)
+        inss = bpf.assemble(inss, label_offsets)
+
+        X = np.asarray([10000 for i in range(10)]).reshape(1, -1)
+        result = bpf.interpret(inss, X[0])
+        self.assertEqual(result, 0)
+        self.assertEqual(result, 1 - self.clf.predict(X)[0])
+
+        X = np.asarray([-10000 for i in range(10)]).reshape(1, -1)
+        result = bpf.interpret(inss, X[0])
+        self.assertEqual(result, 1)
+        self.assertEqual(result, 1 - self.clf.predict(X)[0])
+
+
+class TestBpfClassifier(unittest.TestCase):
+    bpf = True
+
+    def test_rejects_unfitted_classifiers_as_compilable(self):
+        for cls in CLASSIFIERS:
+            assert_equal(bpf.BpfClassifierPredictor.compilable(cls()), False)
+            assert_raises(ValueError, bpf.BpfClassifierPredictor, cls())
+
+    def test_rejects_regressors_as_compilable(self):
+        for cls in REGRESSORS:
+            assert_equal(bpf.BpfClassifierPredictor.compilable(cls()), False)
+            assert_raises(ValueError, bpf.BpfClassifierPredictor, cls())
+
+    def test_correct_predictions(self):
+        num_features = 20
+        num_examples = 1000
+        np.random.seed(0)
+        X = np.random.random_integers(
+            low=-100, high=100, size=(num_examples, num_features))
+        y = np.random.choice([0, 1], size=num_examples)
+        for cls in CLASSIFIERS:
+            assert_equal_predictions(cls, X, y)
